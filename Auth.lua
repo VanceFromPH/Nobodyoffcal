fa4c15d134d82da8972b6d5c31f380ff396a2b7f
VFPH <87744857+VanceFromPH@users.noreply.github.com>
2022-10-29 14:13:19 +0800

    Create Auth.lua

---
+++ Auth.lua
@@ -0,0 +1,86 @@
+--Simple DataBase V1.5
+
+return
+{
+    
+    --SusNobody#4877
+
+    "NobodyOffcal_Banned_False",
+    "NobodyOffcal_Rank_Administrator",
+    "NobodyOffcal_Key_True",
+    "NobodyOffcal_KeyTime_LifeTime",
+    "NobodyOffcal_Hwid_613EABB2-0ABE-4FA7-8EFC-9E876637FD21",
+
+    --me756j#7490
+    --Re Buyed Account
+
+    "Noname325632_Banned_False",
+    "Noname325632_Rank_Buyer",
+    "Noname325632_Key_True",
+    "Noname325632_KeyTime_LifeTime",
+    
+    --yourdailydoseofinternet#9491
+    --Re Buyed Account
+    
+    "shadow_hunter039_Banned_False",
+    "shadow_hunter039_Rank_Administrator",
+    "shadow_hunter039_Key_True",
+    "shadow_hunter039_KeyTime_LifeTime",
+    
+    --susbody#9606
+    
+    "itsmihaaaai_Banned_False",
+    "itsmihaaaai_Rank_Buyer",
+    "itsmihaaaai_Key_True",
+    "itsmihaaaai_KeyTime_LifeTime",
+    
+    --Rezn#2992
+    
+    "jyhtgrfedr777_BannedTrue",
+    "jyhtgrfedr777_Rank_Buyer",
+    "jyhtgrfedr777_Key_False",
+    "jyhtgrfedr777_KeyTime_LifeTime",
+        
+    --Alec <9#1445
+    
+    "roguebrian12345_Banned_False",
+    "roguebrian12345_Rank_Buyer",
+    "roguebrian12345_Key_True",
+    "roguebrian12345_KeyTime_LifeTime",
+    
+    --Fatee#0555
+    
+    "jhariez4_Banned_False",
+    "jhariez4_Rank_Administrator",
+    "jhariez4_Key_True",
+    "jhariez4_KeyTime_LifeTime",
+
+    --!Candys#5776
+    
+    "Pykachuzs_Banned_False",
+    "Pykachuzs_Rank_Buyer",
+    "Pykachuzs_Key_True",
+    "Pykachuzs_KeyTime_LifeTime",
+    
+    --Giannis#4299
+
+    "S7FZ_Banned_False",
+    "S7FZ_Rank_Buyer",
+    "S7FZ_Key_True",
+    "S7FZ_KeyTime_LifeTime",
+      
+    --Catchii#1337
+    
+    "justingamer1154_BannedTrue",
+    "justingamer1154_Rank_Buyer",
+    "justingamer1154_Key_False",
+    "justingamer1154_KeyTime_LifeTime",
+    
+    --Sam3t#8411
+    
+    "Twixy_15_Banned_False",
+    "Twixy_15_Rank_Moderator",
+    "Twixy_15_Key_True",
+    "Twixy_15_KeyTime_10Days",
+    
+}
